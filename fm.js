1ed84cb23e7be58440536ad008615398208a781e
qsqx <894076275@qq.com>
2015-11-18 22:56:29 +0800

    front page 1

--- fm.js
+++ fm.js
@@ -2,7 +2,11 @@ var bkgWid=1920;
 var bkgHgt=940;
 var fixW=0.7;
 var fixH=0.7;
+var bsW=0.30;
+var bsH=0.50;
+var textWid=600;
 function reszBkg(){
+	//resize image
 	var winH=$(window).height();
 	var winW=$(window).width();
 	var w=winW;
@@ -14,6 +18,13 @@ function reszBkg(){
 	var offw=Math.round(fixW*winW-fixW*w);
 	var offh=Math.round(fixH*winH-fixH*h);
 	console.log($(window).width()+'/'+w+','+winH+'/'+h+','+offw+'/'+offh);
-	
 	$('#main-bar').height($(window).height()).css("background-size",w+'px '+h +'px').css("background-position",offw+'px '+offh+'px');
+	w=textWid;
+	h=$('#front-page-container').height();
+	//resize text
+	offw=Math.round(bsW*winW-bsW*w);
+	offw=Math.max(0,offw);
+	offh=Math.round(bsH*winH-bsH*h);
+	$('#front-page-container').css("margin-left",offw+'px');
+	$('#front-page-container').css("margin-top",offh+'px');
 }
