9e416fcf914a05ea70643534e094a90662fab2e3
qsqx <894076275@qq.com>
2015-12-01 14:21:25 +0800

    play audio

---
+++ json.js
@@ -0,0 +1,23 @@
+setList({list:[{
+		title:'【音乐专题】听民谣是一种心情',
+		published:'2015/11/19',
+		duration:'35\'29\"',
+		url:{
+			mp3:'audio/151129.mp3'
+		}
+	},{
+		title:'【故事专题】女生容易被什么感动',
+		published:'2015/11/16',
+		duration:'24\'17\"',
+		url:{
+			mp3: 'audio/123.mp3'
+		}
+	},{
+		title:'【故事专题】我想陪着你，不管从前还是以后',
+		published:'2015/11/14',
+		duration:'26\'31\"',
+		url:{
+			mp3: 'audio/123.mp3'
+		}
+	}
+]});
