45daace68787d7ceac30e5402356c16461605a2a
qsqx <894076275@qq.com>
2015-12-01 11:46:52 +0800

    11:46

--- fm.js
+++ fm.js
@@ -5,6 +5,8 @@ var fixH=0.7;
 var bsW=0.30;
 var bsH=0.50;
 var textWid=500;
+var arr;
+var playnw;
 function reszBkg(){
 	//resize image
 	$('.frontpage').css({'width':$(window).width()+'px','height':$(window).height()+'px'});
@@ -27,7 +29,38 @@ function reszBkg(){
 	offw=Math.max(0,offw);
 	offh=Math.round(bsH*winH-bsH*h);
 	offh=Math.max(0,offh);
-	console.log({'padding-left':offw+'px','padding-top':offh+'px'});
 	$('.frontpage').css({'padding-left':offw+'px','padding-top':offh+'px'});
 	//$('#frontpage-label-container').css({'margin-left':offw+'px','margin-top':offh+'px'});
 }
+$(document).ready(function(){
+	$('#spread').on('click',function(){
+		$(window).off('resize');
+		var w=$(window).width();
+		w=Math.max(w,1320);
+		w=w-1280;
+		$('#main-container').css('min-width','1320px');
+		$('.frontpage-container').animate({'width':'320px','height':'600px','margin-top':'18px','margin-left':w*0.56+960+'px','border-radius':'8px'},'slow','swing',function(){
+			$('#front-page-container').animate({'background-size':'240px 240px','background-position':'30px 45px'},'slow');			
+			$('.frontpage').css({'padding':'0px','width':'100%','height':'100%'}).animate(
+				{'width':'240px','height':'240px','margin':'40px'},'slows','swing',function(){
+					$('.frontpage').css({'background-size':'240px 240px','background-position':'0px 0px','background-image':'url(17105288983053444_320x320.png)'});
+					$('.profile-detail').fadeIn('slow',function(){
+						$('.frontpage-container').css('margin-left',w*0.12+'px');
+						$('#list-container').fadeIn('slow');
+						$(window).on('resize',function(){
+							var w=$(window).width();
+							w=Math.max(w,1320);
+							w=w-1280;
+							$('#list-container').css('margin-left',w*0.44+'px');
+							$('.frontpage-container').css('margin-left',w*0.12+'px');
+						}).resize();
+					});
+				});						
+		});
+		$('#frontpage-label-container').remove();
+	});
+}).ready(function(){
+	$(window).on('resize',reszBkg);
+}).ready(reszBkg).ready(function(){
+	//$('#spread').click();
+});
