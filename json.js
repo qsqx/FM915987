589b1ce7262fd816865579b7fa40500e5c315c92
qsqx <894076275@qq.com>
2015-12-02 12:38:21 +0800

    lunch

--- json.js
+++ json.js
@@ -10,14 +10,14 @@ setList({list:[{
 		published:'2015/11/16',
 		duration:'24\'17\"',
 		url:{
-			mp3: 'audio/123.mp3'
+			mp3: 'audio/George Bruns - Cat\'s Meow.mp3'
 		}
 	},{
 		title:'【故事专题】我想陪着你，不管从前还是以后',
 		published:'2015/11/14',
 		duration:'26\'31\"',
 		url:{
-			mp3: 'audio/123.mp3'
+			
 		}
 	}
 ]});
